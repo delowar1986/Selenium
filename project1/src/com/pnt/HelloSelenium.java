4c45a43760a2c3d7814296c6c98bca5a30e8190e
delowar86 <delowar1986@gmailcom>
2013-12-30 22:01:04 -0500

    hellow java

---
+++ project1/src/com/pnt/HelloSelenium.java
@@ -0,0 +1,11 @@
+package com.pnt;
+
+public class HelloSelenium {
+
+	public static void main(String[] args) {
+		System.out.println("hello java/selenium class");
+			
+
+	}
+
+}
---
+++ project1/src/com/pnt/PntJavaSession.java
@@ -0,0 +1,11 @@
+package com.pnt;
+
+public class PntJavaSession {
+
+	public static void main(String[] args) {
+        System.out.println("hello java");
+ 
+    }
+ 
+
+}
